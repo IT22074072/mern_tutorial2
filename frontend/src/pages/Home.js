58b05f357d7a4a7bf500d80d8248055ca66cf3c7
IT22074072 <it22074072@my.sliit.lk>
2024-12-09 15:19:15 +0530

    fetching workouts to the home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails";
 
 function Home() {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      try {
+        const response = await fetch('/api/workouts')
+        const data = await response.json(); // Parse the response as JSON, now we have workout objects
+        console.log(data.data);
+        setWorkouts(data.data);
+      } catch (error) {
+        console.error("Error in fetching workouts", error);
+      }
+    };
+
+    fetchWorkouts();
+  }, []); //fire only once when render the home component
+
   return (
-    <div>Home</div>
+    <div className="home">
+        <div className="workouts">
+            {workouts && workouts.map((workout)=>(
+                <WorkoutDetails key={workout._id} workout={workout} />
+            ))}
+
+        </div>
+
+    </div>
   )
 }
 
-export default Home
+export default Home;
