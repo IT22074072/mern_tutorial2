b93077bf1e092165619023528ac38856ae0c2f84
IT22074072 <it22074072@my.sliit.lk>
2024-12-10 17:30:22 +0530

    create useContext hook

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { WorkoutsContextProvider } from "./context/WokoutContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <WorkoutsContextProvider>
+      <App />  {/*children of WorkoutsContextProvider*/}
+    </WorkoutsContextProvider>
   </React.StrictMode>
 );
-
-
